19375031fe6362f4fa711261bae36e4c2f80a25b
Cyril Glapa <cyril.glapa@keuse.fr>
2013-06-25 14:37:51 +0200

    Adding Base64FileField to fields

--- tastypie/fields.py
+++ tastypie/fields.py
@@ -1,10 +1,13 @@
+import base64
 import datetime
+import os
 from dateutil.parser import parse
 from decimal import Decimal
 import re
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.utils import datetime_safe, importlib
 from django.core.urlresolvers import resolve
+from django.core.files.uploadedfile import SimpleUploadedFile
 from tastypie.bundle import Bundle
 from tastypie.exceptions import ApiFieldError, NotFound
 from tastypie.utils import dict_strip_unicode_keys, make_aware
@@ -214,6 +217,52 @@ class FileField(ApiField):
             return getattr(value, 'url', value)
         except ValueError:
             return None
+
+
+class Base64FileField(FileField):
+    """
+    A django-tastypie field for handling file-uploads through raw post data.
+    It uses base64 for en-/decoding the contents of the file.
+    Usage:
+
+    class MyResource(ModelResource):
+        file_field = Base64FileField("file_field")
+        
+        class Meta:
+            queryset = ModelWithFileField.objects.all()
+
+    In the case of multipart for submission, it would also pass the filename.
+    By using a raw post data stream, we have to pass the filename within our
+    file_field structure:
+
+    file_field = {
+        "name": "myfile.png",
+        "file": "longbas64encodedstring",
+        "content_type": "image/png" # on hydrate optional
+    }
+    """
+    def dehydrate(self, bundle):
+        if not bundle.data.has_key(self.instance_name) and hasattr(bundle.obj, self.instance_name):
+            file_field = getattr(bundle.obj, self.instance_name)
+            if file_field:
+                try:
+                    content_type, encoding = mimetypes.guess_type(file_field.file.name)
+                    b64 = open(file_field.file.name, "rb").read().encode("base64")
+                    ret = {
+                        "name": os.path.basename(file_field.file.name),
+                        "file": b64,
+                        "content-type": content_type or "application/octet-stream"
+                    }
+                    return ret
+                except:
+                    pass
+        return None
+
+    def hydrate(self, obj):
+        value = super(FileField, self).hydrate(obj)
+        if value:
+            value = SimpleUploadedFile(value["name"], base64.b64decode(value["file"]), getattr(value, "content_type", "application/octet-stream"))
+        return value
 
 
 class IntegerField(ApiField):
